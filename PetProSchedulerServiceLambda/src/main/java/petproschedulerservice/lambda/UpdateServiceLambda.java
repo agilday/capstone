36df5c12d6bdf7a02d2857a310701f933c313037
Ashley Gilday <ashleybrookrobinson@gmail.com>
2023-06-08 15:54:30 -0500

    updateservicelambda

--- PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/lambda/UpdateServiceLambda.java
+++ PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/lambda/UpdateServiceLambda.java
@@ -1,4 +1,26 @@
-//package petproschedulerservice.lambda;
-//
-//public class UpdateServiceMenuLambda {
-//}
+package petproschedulerservice.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import petproschedulerservice.activity.requests.UpdateServiceRequest;
+import petproschedulerservice.activity.results.UpdateServiceResult;
+
+public class UpdateServiceLambda
+        extends LambdaActivityRunner<UpdateServiceRequest, UpdateServiceResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<UpdateServiceRequest>, LambdaResponse> {
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<UpdateServiceRequest> input, Context context) {
+        return super.runActivity(
+                () -> {
+                    UpdateServiceRequest unauthenticatedRequest = input.fromBody(UpdateServiceRequest.class);
+                    return input.fromUserClaims(claims ->
+                            UpdateServiceRequest.builder()
+                                    .withTitle(unauthenticatedRequest.getTitle())
+                                    .withDescription(unauthenticatedRequest.getDescription())
+                                    .build());
+                },
+                (request, serviceComponent) ->
+                        serviceComponent.provideUpdateServiceActivity().handleRequest(request)
+        );
+    }
+}
