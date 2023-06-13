8bea98cca0e0f8f8f0b1e25b78be35ad00096a63
Ashley Gilday <ashleybrookrobinson@gmail.com>
2023-06-13 14:09:23 -0500

    getallappointmentsactivity test

--- PetProSchedulerServiceLambda/src/test/java/GetAllAppointmentsActivityTest.java
+++ PetProSchedulerServiceLambda/src/test/java/GetAllAppointmentsActivityTest.java
@@ -1,2 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import petproschedulerservice.activity.GetAllAppointmentsActivity;
+import petproschedulerservice.activity.requests.GetAllAppointmentsRequest;
+import petproschedulerservice.activity.results.GetAllAppointmentsResult;
+import petproschedulerservice.dynamodb.AppointmentDao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 public class GetAllAppointmentsActivityTest {
+    @Mock
+    private AppointmentDao appointmentDao;
+
+    private GetAllAppointmentsActivity getAllAppointmentsActivity;
+
+    @BeforeEach
+    public void setup() {
+        initMocks(this);
+        getAllAppointmentsActivity = new GetAllAppointmentsActivity(appointmentDao);
+    }
+
+    @Test
+    public void handleRequest_getsAllAppointments() {
+        //GIVEN
+        GetAllAppointmentsRequest request = new GetAllAppointmentsRequest();
+
+        //WHEN
+        GetAllAppointmentsResult result = getAllAppointmentsActivity.handleRequest();
+
+        //THEN
+        assertNotNull(result.getAllAppointmentsList());
+    }
 }
