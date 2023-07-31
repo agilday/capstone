be70839ffd49209916dfcb32ea99dc48b962845e
Ashley Gilday <ashleybrookrobinson@gmail.com>
2023-07-31 11:13:37 -0500

    gp basics implemented

--- PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/dynamodb/GroomerProfile.java
+++ PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/dynamodb/GroomerProfile.java
@@ -1,4 +1,102 @@
 package petproschedulerservice.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.List;
+import java.util.Objects;
+
+@DynamoDBTable(tableName="groomerprofiles")
+
+
 public class GroomerProfile {
+    private String lname;
+    private String fname;
+    private String phone;
+    private String availability;
+    private List<String> notes;
+    private Boolean groomsCats;
+
+    @DynamoDBHashKey(attributeName = "lname")
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    @DynamoDBRangeKey(attributeName = "fname")
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    @DynamoDBAttribute(attributeName = "phone")
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @DynamoDBAttribute(attributeName = "availability")
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    @DynamoDBAttribute(attributeName = "notes")
+    public List<String> getNotes() {
+        return notes;
+    }
+
+    public void addNote(String note) {
+        this.notes.add(note);
+    }
+
+    @DynamoDBAttribute(attributeName = "groomsCats")
+    public Boolean getGroomsCats() {
+        return groomsCats;
+    }
+
+    public void setGroomsCats(Boolean groomsCats) {
+        this.groomsCats = groomsCats;
+    }
+
+    public void setNotes(List<String> notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GroomerProfile profile = (GroomerProfile) o;
+        return lname.equals(profile.lname) &&
+                fname.equals(profile.fname) &&
+                phone.equals(profile.phone) &&
+                availability.equals(profile.availability) &&
+                notes.equals(profile.notes) &&
+                groomsCats.equals(profile.groomsCats);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lname, fname, phone, availability, notes, groomsCats);
+    }
+
 }
--- PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/dynamodb/GroomerProfileDao.java
+++ PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/dynamodb/GroomerProfileDao.java
@@ -1,4 +1,115 @@
 package petproschedulerservice.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import petproschedulerservice.exceptions.ProfileNotFoundException;
+import petproschedulerservice.metrics.MetricsConstants;
+import petproschedulerservice.metrics.MetricsPublisher;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Singleton
 public class GroomerProfileDao {
+    private final DynamoDBMapper dynamoDbMapper;
+    private final MetricsPublisher metricsPublisher;
+
+    /**
+     * Instantiates a ClientProfileDao object.
+     *
+     * @param dynamoDbMapper   the {@link DynamoDBMapper} used to interact with the profiles table
+     * @param metricsPublisher the {@link MetricsPublisher} used to record metrics.
+     */
+    @Inject
+    public GroomerProfileDao(DynamoDBMapper dynamoDbMapper, MetricsPublisher metricsPublisher) {
+        this.dynamoDbMapper = dynamoDbMapper;
+        this.metricsPublisher = metricsPublisher;
+    }
+
+    /**
+     * Returns the {@link GroomerProfile} corresponding to the specified lname.
+     *
+     * @param lname the Profile last name
+     * @return the stored GroomerProfile, or null if none was found.
+     */
+    public GroomerProfile getGroomerProfile(String lname) {
+        GroomerProfile profile = this.dynamoDbMapper.load(GroomerProfile.class, lname);
+
+        if (profile == null) {
+            metricsPublisher.addCount(MetricsConstants.GETPROFILE_PROFILENOTFOUND_COUNT, 1);
+            throw new ProfileNotFoundException("Could not find profile with lname " + lname);
+        }
+        metricsPublisher.addCount(MetricsConstants.GETPROFILE_PROFILENOTFOUND_COUNT, 0);
+        return profile;
+    }
+
+    public List<GroomerProfile> getAllGroomerProfiles() {
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
+        List<GroomerProfile> groomerProfilesList = dynamoDbMapper.scan(GroomerProfile.class, scanExpression);
+        return groomerProfilesList;
+    }
+
+    /**
+     * Saves (creates or updates) the given profile.
+     *
+     * @param GroomerProfile profile to save
+     * @return The GroomerProfile object that was saved
+     */
+    public void saveGroomerProfile(GroomerProfile profile) {
+        this.dynamoDbMapper.save(profile);
+    }
+
+    /**
+     * Perform a search (via a "scan") of the profiles table for profiles matching the given criteria.
+     *
+     * Both "profileName" and "tags" attributes are searched.
+     * The criteria are an array of Strings. Each element of the array is search individually.
+     * ALL elements of the criteria array must appear in the profileName or the tags (or both).
+     * Searches are CASE SENSITIVE.
+     *
+     * @param criteria an array of String containing search criteria.
+     * @return a List of ClientProfile objects that match the search criteria.
+     */
+    public List<GroomerProfile> searchGroomerProfile(String[] criteria) {
+        DynamoDBScanExpression dynamoDBScanExpression = new DynamoDBScanExpression();
+
+        if (criteria.length > 0) {
+            Map<String, AttributeValue> valueMap = new HashMap<>();
+            String valueMapNamePrefix = ":c";
+
+            StringBuilder nameFilterExpression = new StringBuilder();
+            StringBuilder tagsFilterExpression = new StringBuilder();
+
+            for (int i = 0; i < criteria.length; i++) {
+                valueMap.put(valueMapNamePrefix + i,
+                        new AttributeValue().withS(criteria[i]));
+                nameFilterExpression.append(
+                        filterExpressionPart("profileName", valueMapNamePrefix, i));
+                tagsFilterExpression.append(
+                        filterExpressionPart("tags", valueMapNamePrefix, i));
+            }
+
+            dynamoDBScanExpression.setExpressionAttributeValues(valueMap);
+            dynamoDBScanExpression.setFilterExpression(
+                    "(" + nameFilterExpression + ") or (" + tagsFilterExpression + ")");
+        }
+
+        return this.dynamoDbMapper.scan(GroomerProfile.class, dynamoDBScanExpression);
+    }
+
+    private StringBuilder filterExpressionPart(String target, String valueMapNamePrefix, int position) {
+        String possiblyAnd = position == 0 ? "" : "and ";
+        return new StringBuilder()
+                .append(possiblyAnd)
+                .append("contains(")
+                .append(target)
+                .append(", ")
+                .append(valueMapNamePrefix).append(position)
+                .append(") ");
+    }
 }
+
