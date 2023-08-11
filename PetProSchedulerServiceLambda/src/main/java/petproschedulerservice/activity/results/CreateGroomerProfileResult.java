8629ab1fb521d8548f651ba918d97a7383555106
Ashley Gilday <ashleybrookrobinson@gmail.com>
2023-08-11 17:16:43 -0500

    create grooer profile result

--- PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/activity/results/CreateGroomerProfileResult.java
+++ PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/activity/results/CreateGroomerProfileResult.java
@@ -1,4 +1,40 @@
 package petproschedulerservice.activity.results;
 
+import petproschedulerservice.models.GroomerProfileModel;
+
 public class CreateGroomerProfileResult {
+    private final GroomerProfileModel profile;
+
+    private CreateGroomerProfileResult(GroomerProfileModel profile) {
+        this.profile = profile;
+    }
+
+    public GroomerProfileModel getProfile() {
+        return profile;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateGroomerProfileResult{" +
+                "groomerprofile=" + profile +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static CreateGroomerProfileResult.Builder builder() {
+        return new CreateGroomerProfileResult.Builder();
+    }
+
+    public static class Builder {
+        private GroomerProfileModel profile;
+
+        public CreateGroomerProfileResult.Builder withProfile(GroomerProfileModel profile) {
+            this.profile = profile;
+            return this;
+        }
+
+        public CreateGroomerProfileResult build() {
+            return new CreateGroomerProfileResult(profile);
+        }
+    }
 }
