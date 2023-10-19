b4dd9cf4f70d72d5213876530fb35d11a43b1f72
Ashley Gilday <ashleybrookrobinson@gmail.com>
2023-10-19 16:56:55 -0500

    delete groomer profile lambda

--- PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/lambda/DeleteGroomerProfileLambda.java
+++ PetProSchedulerServiceLambda/src/main/java/petproschedulerservice/lambda/DeleteGroomerProfileLambda.java
@@ -1,4 +1,28 @@
 package petproschedulerservice.lambda;
 
-public class DeleteGroomerProfileLambda {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import petproschedulerservice.activity.requests.DeleteGroomerProfileRequest;
+import petproschedulerservice.activity.results.DeleteGroomerProfileResult;
+
+public class DeleteGroomerProfileLambda extends LambdaActivityRunner<DeleteGroomerProfileRequest, DeleteGroomerProfileResult>
+        implements RequestHandler<AuthenticatedLambdaRequest<DeleteGroomerProfileRequest>, LambdaResponse> {
+    /**
+     * Handles a Lambda Function request
+     *
+     * @param input   The Lambda Function input
+     * @param context The Lambda execution environment context object.
+     * @return The Lambda Function output
+     */
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<DeleteGroomerProfileRequest> input, Context context) {
+        return super.runActivity(
+                () -> input.fromPath(path ->
+                        DeleteGroomerProfileRequest.builder()
+                                .withLname(path.get("lname"))
+                                .build()),
+                (request,serviceComponent)->
+                        serviceComponent.provideDeleteGroomerProfileActivity().handleRequest(request));
+    }
+
 }
